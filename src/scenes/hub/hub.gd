5c7d4cc63a0bef2bc16ba59f74e3c261a1e45953
Shinigami92 <chrissi92@hotmail.de>
2026-04-25 12:11:07 +0200

    refactor: rename arena → zone

--- src/scenes/hub/hub.gd
+++ src/scenes/hub/hub.gd
@@ -18,7 +18,7 @@ func _ready() -> void:
 func _process(_delta: float) -> void:
 	if player_in_terminal and Input.is_action_just_pressed("interact"):
 		SaveManager.save_game()
-		SceneManager.go_to_arena()
+		SceneManager.go_to_zone()
 
 
 func _spawn_player() -> void:
